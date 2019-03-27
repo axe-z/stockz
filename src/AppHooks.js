408598db8c55c7765d41af5062b8cc3cac96c948
axe-z <info@axe-z.com>
2019-03-27 12:57:49 -0400

    ajout de hook sur class async

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -1,11 +1,11 @@
-import React,{  useState, useEffect } from 'react';
+import React,{ useState,useEffect } from 'react';
 import Header from './components/Header/Header';
 import ListView from './components/ListView/ListView';
-import { retrieveBusinessInfo, retrieveBusinessData } from './utilities';
+import { retrieveBusinessInfo,retrieveBusinessData } from './utilities';
 
 
-function AppHooks (props){
-  const [state, setState] = useState({
+function AppHooks (props) {
+  const [state,setState] = useState({
     stockInput: '',
     value: '10',
     socialValue: "twitter",
@@ -16,91 +16,95 @@ function AppHooks (props){
     isLoading: false
   });
 
- const handleChange = event => {
+  const handleChange = event => {
     const { name,value } = event.target;
-    setState({ ...state, [name]: value }); //pas de merge, donc reconstruit le state.
+    setState({ ...state,[name]: value }); //pas de merge, donc reconstruit le state.
   };
 
-//remplace cdm
-useEffect(async() => {
-  const stockName = await retrieveBusinessInfo();
-  setState({ ...state,stockName });
- },[])
+  //remplace cdm
+  useEffect(() => {
+    async function fetchData () {
+      // You can await here
+      const stockName = await retrieveBusinessInfo();
+      setState({ ...state,stockName });
+    }
+    fetchData();
+  },[])
 
   //va chercher le data qui viendrait d un backend au lieu d un api gratuit.
   //2 situations, on veut 1 seul call par entreprise. donc si present on garde.
   const stockPriceGenerator = async (e) => {
     e.preventDefault();
 
-     const {stockInput, value, stockInitial, stockFull } =  state;
+    const { stockInput,value,stockInitial,stockFull } = state;
 
     //SI PREMIERE RECHERCHE
     if (stockInitial !== stockInput) {
       // console.log('full')
       const stockFull = await retrieveBusinessData(stockInput);
-      const stockData = [...stockFull].slice(0, value);
-        //prend tout et qui vaut le nom
-        setState({ ...state, stockFull, stockData, stockInitial: stockInput });
+      const stockData = [...stockFull].slice(0,value);
+      //prend tout et qui vaut le nom
+      setState({ ...state,stockFull,stockData,stockInitial: stockInput });
 
     } else {  //SI DEJA UNE ENTREPRISE AU TABLEAU
       // console.log('actuelle')
-      const stockData = [...stockFull].slice(0, value);
-      setState({ ...state, stockData });
+      const stockData = [...stockFull].slice(0,value);
+      setState({ ...state,stockData });
     }
   }
 
-    return (
-      <div className="App">
-        {/* Header component*/}
-        <Header stockName={state.stockName} />
+  return (
+    <div className="App">
+      {/* Header component*/}
+      <Header stockName={state.stockName} />
 
 
-        {/* main app*/}
-        <div className="mainApp">
-          <h3 className="mission">Validating the effects of social media on brand valuation</h3>
-          <h5>- in 3 simple steps -</h5>
+      {/* main app*/}
+      <div className="mainApp">
+        <h3 className="mission">Validating the effects of social media on brand valuation</h3>
+        <h5>- in 3 simple steps -</h5>
 
-          {/* form */}
-          <form onSubmit={stockPriceGenerator} >
-            <div className="cover">
-              <h2 className="num">01/ <span> enter the symbol</span></h2>
-              <div className="search">
-                <label htmlFor="stockInput" className="inp">
-                  <input type="text" name="stockInput" value={state.stockInput} onChange={handleChange}
+        {/* form */}
+        <form onSubmit={stockPriceGenerator} >
+          <div className="cover">
+            <h2 className="num">01/ <span> enter the symbol</span></h2>
+            <div className="search">
+              <label htmlFor="stockInput" className="inp">
+                <input type="text" name="stockInput" value={state.stockInput} onChange={handleChange}
                   id="stockInput" className="searchInput" autoComplete="off" spellCheck="false" tabIndex="1" />
-                  <span className="label" style={state.stockInput ? { opacity: 0 } : null}>search...</span>
-                </label>
-                <div className="wrapSelect">
-                  <h2 className="num">02/ <span> days reviewed</span></h2>
-                  <select name="value" value={state.value} onChange={handleChange} tabIndex="2">
-                    <option value="10">10 days</option>
-                    <option value="20">20 days</option>
-                    <option value="30">30 days</option>
-                    <option value="60">60 days</option>
-                    <option value="90">90 days</option>
-                  </select>
-                </div>
-                <div className="wrapSelectSocial">
-                  <h2 className="num">03/ <span> by social Media</span></h2>
-                  <select name="socialValue" value={state.socialValue} onChange={handleChange} tabIndex="3">
-                    <option value="twitter">twitter</option>
-                    <option value="facebook">facebook</option>
-                  </select>
-                </div>
+                <span className="label" style={state.stockInput ? { opacity: 0 } : null}>search...</span>
+              </label>
+              <div className="wrapSelect">
+                <h2 className="num">02/ <span> days reviewed</span></h2>
+                <select name="value" value={state.value} onChange={handleChange} tabIndex="2">
+                  <option value="10">10 days</option>
+                  <option value="20">20 days</option>
+                  <option value="30">30 days</option>
+                  <option value="60">60 days</option>
+                  <option value="90">90 days</option>
+                </select>
+              </div>
+              <div className="wrapSelectSocial">
+                <h2 className="num">03/ <span> by social Media</span></h2>
+                <select name="socialValue" value={state.socialValue} onChange={handleChange} tabIndex="3">
+                  <option value="twitter">twitter</option>
+                  <option value="facebook">facebook</option>
+                </select>
               </div>
             </div>
-            <button type="submit" className="btnProjet" disabled={!state.stockInput} tabIndex="4">Go !</button>
-          </form>
-        </div>
-
-        {/* list component*/}
-
-       	  {state.stockData.length > 0 && <ListView data={state.stockData} socialValue={state.socialValue}/>}
-
-
+          </div>
+          <button type="submit" className="btnProjet" disabled={!state.stockInput} tabIndex="4">Go !</button>
+        </form>
       </div>
-    );
-  }
+
+      {/* list component*/}
+
+      {state.stockData.length > 0 && <ListView data={state.stockData} socialValue={state.socialValue} />}
+
+
+    </div>
+  );
+}
 
 export default AppHooks;
 
